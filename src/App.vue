1957aa6e9862dc5f7f2f855ebea02d76741e7f83
bugraYvz99 <you@example.com>
2023-07-10 05:16:35 +0300

    layout designed

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MainLayout from './components/MainLayout.vue'
+
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
+
+<template>
+  <MainLayout />
+</template>
